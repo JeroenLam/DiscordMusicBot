2d6a5c309ed3ee8c29c7b6e0bf7e1dcbd0e5dbbc
Jeroen <jeroenlammers1@gmail.com>
2022-01-02 16:00:18 +0100

    removed import of deleted file

--- app/MusicBot/CommandHandler.py
+++ app/MusicBot/CommandHandler.py
@@ -12,7 +12,6 @@ from MusicBot.Commands.removeComands import *
 from MusicBot.Commands.skipCommand import SkipCommand
 from MusicBot.Commands.addRandomCommand import AddRandom2QueueCommand
 from MusicBot.Commands.listMusicCommand import ListMusicCommand
-from MusicBot.Commands.addStreamCommand import AddStreamCommand
 
 class CommandHandler(BaseCommandHandler):
     # On initialisation, define all commands in the command handler
